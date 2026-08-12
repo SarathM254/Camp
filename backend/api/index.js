6ec904049091fd74e47885e04017ac77b94899b3
M.Sarath <motupallisarathchandra@gmail.com>
2026-08-12 16:11:45 +0530

    Deployment

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,7 @@
 import app from '../server.js';
+import connectDB from '../config/db.js';
+
+// Ensure DB is connected for serverless invocations
+connectDB().catch(console.error);
 
 export default app;
